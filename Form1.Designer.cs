8d55c628ce8790ecff31787269b1dc6c57a06b3d
ChristianK01 <ChristianKenny01@gmail.com>
2017-07-15 16:22:25 -0300

    Added Files

---
+++ Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace HangManGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Hang_Man = new System.Windows.Forms.GroupBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.Unknown_Word = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Submit_Answers = new System.Windows.Forms.GroupBox();
+            this.Submit_letter_box = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.Hang_Man.SuspendLayout();
+            this.Unknown_Word.SuspendLayout();
+            this.Submit_Answers.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Hang_Man
+            // 
+            this.Hang_Man.Controls.Add(this.panel1);
+            this.Hang_Man.Location = new System.Drawing.Point(12, 12);
+            this.Hang_Man.Name = "Hang_Man";
+            this.Hang_Man.Size = new System.Drawing.Size(203, 258);
+            this.Hang_Man.TabIndex = 0;
+            this.Hang_Man.TabStop = false;
+            this.Hang_Man.Text = "Hang Man";
+            // 
+            // panel1
+            // 
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(3, 16);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(197, 239);
+            this.panel1.TabIndex = 0;
+            // 
+            // Unknown_Word
+            // 
+            this.Unknown_Word.Controls.Add(this.label2);
+            this.Unknown_Word.Controls.Add(this.label1);
+            this.Unknown_Word.Location = new System.Drawing.Point(235, 28);
+            this.Unknown_Word.Name = "Unknown_Word";
+            this.Unknown_Word.Size = new System.Drawing.Size(284, 133);
+            this.Unknown_Word.TabIndex = 1;
+            this.Unknown_Word.TabStop = false;
+            this.Unknown_Word.Text = "Unknown Word";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(93, 20);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Wrong Letters:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Word Length:";
+            // 
+            // Submit_Answers
+            // 
+            this.Submit_Answers.Controls.Add(this.Submit_letter_box);
+            this.Submit_Answers.Controls.Add(this.button1);
+            this.Submit_Answers.Location = new System.Drawing.Point(235, 176);
+            this.Submit_Answers.Name = "Submit_Answers";
+            this.Submit_Answers.Size = new System.Drawing.Size(284, 94);
+            this.Submit_Answers.TabIndex = 2;
+            this.Submit_Answers.TabStop = false;
+            this.Submit_Answers.Text = "Submit Letter";
+            // 
+            // Submit_letter_box
+            // 
+            this.Submit_letter_box.Location = new System.Drawing.Point(126, 65);
+            this.Submit_letter_box.MaxLength = 1;
+            this.Submit_letter_box.Name = "Submit_letter_box";
+            this.Submit_letter_box.Size = new System.Drawing.Size(44, 20);
+            this.Submit_letter_box.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(23, 53);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(97, 32);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Submit Letter";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(547, 287);
+            this.Controls.Add(this.Submit_Answers);
+            this.Controls.Add(this.Unknown_Word);
+            this.Controls.Add(this.Hang_Man);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "HangMan";
+            this.Shown += new System.EventHandler(this.Form1_Shown);
+            this.Hang_Man.ResumeLayout(false);
+            this.Unknown_Word.ResumeLayout(false);
+            this.Unknown_Word.PerformLayout();
+            this.Submit_Answers.ResumeLayout(false);
+            this.Submit_Answers.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox Hang_Man;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.GroupBox Unknown_Word;
+        private System.Windows.Forms.GroupBox Submit_Answers;
+        private System.Windows.Forms.TextBox Submit_letter_box;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+
+namespace HangManGame
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //Variables to be used throughout the program
+        string hidden_word = "";
+        List<Label> unknown_letters = new List<Label>();
+        int guesses = 0;
+
+        private void Form1_Shown(object sender, EventArgs e) //Calls methods
+        {
+            Hangman_Post();
+            Draw_UnknownLetters();
+        }
+
+        void Reset() //Method to reset board on a win or lose condition
+        {
+            Graphics g = panel1.CreateGraphics();
+            g.Clear(Panel.DefaultBackColor);
+            RandomWord();
+            Draw_UnknownLetters();
+            Hangman_Post();
+            label2.Text = "Wrong Letters: ";
+            Submit_letter_box.Text = "";
+        }
+
+        string RandomWord() //Selects a random word from the URL. Places that word in an array.
+        {
+            WebClient wc = new WebClient();
+            string wordList = wc.DownloadString
+            ("https://raw.githubusercontent.com/Tom25/Hangman/master/wordlist.txt");
+
+            string[] selected_word = wordList.Split('\n'); //Separates words by a new line
+            Random random_word = new Random(); // Picks a word at random
+            return selected_word[random_word.Next(0, selected_word.Length - 1)]; //Returns word in the array
+        }
+
+        void Draw_UnknownLetters() // Creates spaces in the panel to indicate unknown letters of the random word
+        {
+            hidden_word = RandomWord();
+            char[] word = hidden_word.ToCharArray();
+            int between = 275 / word.Length - 1; // Creates spaces based on the position on the Unknown Word panel
+            for (int i = 0; i < word.Length; i++)
+            {
+                unknown_letters.Add(new Label());
+                unknown_letters[i].Location = new Point((i * between) + 10, 80); // calculates position of next space
+                unknown_letters[i].Text = "_"; //Default marker for unknown letter. Is replaced by letter on correct guess.
+                unknown_letters[i].Parent = Unknown_Word;
+                unknown_letters[i].BringToFront(); //Makes sure you can always see spaces by bringing them to the front
+                unknown_letters[i].CreateControl();
+            }
+            label1.Text = "Word Length: " + (word.Length).ToString();
+        }
+
+        enum BodyParts //Creates body parts that will be drawn if a wrong letter or word is selected
+        {
+            Head,
+            Body,
+            R_Arm,
+            L_Arm,
+            R_Leg,
+            L_Leg,
+            L_Eye,
+            R_Eye,
+            Mouth,
+        }
+
+        void Hangman_Post() // Draws hangman post when the program loads
+        {
+            Graphics hangPost = panel1.CreateGraphics();
+            Pen pen = new Pen(Color.Black, 10);
+            hangPost.DrawLine(pen, new Point(160, 275), new Point(160, 5)); //Coordinates of the post in the panel
+            hangPost.DrawLine(pen, new Point(165, 5), new Point(95, 5));
+            hangPost.DrawLine(pen, new Point(90, 0), new Point(90, 50));
+           
+        }
+
+        void DrawBodyParts(BodyParts BP) // Creates body parts that will be revealed on a wrong guess
+        {
+            Graphics g = panel1.CreateGraphics();
+            Pen p = new Pen(Color.Black, 2);
+            if (BP == BodyParts.Head)
+                g.DrawEllipse(p, 65, 50, 50, 50);
+
+            else if (BP == BodyParts.L_Eye)
+            {
+                SolidBrush s = new SolidBrush(Color.Black);
+                g.FillEllipse(s, 77, 70, 5, 5);
+            }
+            else if (BP == BodyParts.R_Eye)
+            {
+                SolidBrush s = new SolidBrush(Color.Black);
+                g.FillEllipse(s, 94, 70, 5, 5);
+            }
+            else if (BP == BodyParts.Mouth)
+            {
+                g.DrawArc(p, 77, 85, 25, 25, 225, 90);
+            }
+            else if (BP == BodyParts.Body)
+            {
+                g.DrawLine(p, new Point(90, 100), new Point(90, 188));
+            }
+            else if (BP == BodyParts.L_Arm)
+            {
+                g.DrawLine(p, new Point(90, 125), new Point(50, 110));
+            }
+            else if (BP == BodyParts.R_Arm)
+            {
+                g.DrawLine(p, new Point(90, 125), new Point(125, 110));
+            }
+            else if (BP == BodyParts.L_Leg)
+            {
+                g.DrawLine(p, new Point(90, 185), new Point(65, 230));
+            }
+            else if (BP == BodyParts.R_Leg)
+            {
+                g.DrawLine(p, new Point(89, 185), new Point(115, 230));
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            char letter = Submit_letter_box.Text.ToLower().ToCharArray()[0];  // user submission will always be lower case letter
+            if (!char.IsLetter(letter))
+            {
+                MessageBox.Show("Invalid. Only letters accepted", "Wrong Input!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            if (hidden_word.Contains(letter))
+            {
+                char[] letters = hidden_word.ToCharArray(); //Places hidden word into an array separated by letters
+                for (int i = 0; i < letters.Length; i++)
+                {
+                    if (letters[i] == letter) //Changes blank spaces with correct letter if the user is correct.
+                        unknown_letters[i].Text = letter.ToString();
+                }
+            }
+            else
+            {
+                MessageBox.Show("That letter is not in the word.", "Incorrect");
+                label2.Text += " " + letter.ToString() + ",";
+                DrawBodyParts((BodyParts)guesses); //Assigns body parts to an int, and draws body part on wrong guess 
+                guesses++;
+                if (guesses == 9)
+                {
+                    MessageBox.Show("You lost! The word was: " + hidden_word);
+                    Reset();
+                }
+                foreach (Label l in unknown_letters) 
+                    if (l.Text == "_") return;
+                MessageBox.Show("You have won!!! The word was: " + hidden_word, "You have won!!!");
+                Reset();
+            }
+        }
+    }
+}
